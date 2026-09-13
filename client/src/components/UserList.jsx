48e2643b0a1df3e6e52a43668cdce10e492d0cea
Metodi Sukalinski <msukalinski@gmail.com>
2026-09-13 20:05:47 +0300

    Show dynamic users

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -175,7 +175,7 @@ export default function UserList() {
                     </tr>
                 </thead>
                 <tbody>
-                    <UserItem />
+                    {users.map(user => <UserItem key={user._id} {...user} />)}
                 </tbody>
             </table>
 
